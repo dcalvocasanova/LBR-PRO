9a535892a49b169d4c093264791f88d17e25e6e9
dcalvocasanova <dcalvocasanova@gmail.com>
2019-10-08 22:57:06 -0600

    Revert "Hagem"

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Security\SecurityBL;
 use App\Http\Controllers\Controller;
-use App\Enums\LoginResult;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,6 +21,13 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
      * Create a new controller instance.
      *
      * @return void
@@ -33,26 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-
-    public function login(Request $request)
-    {
-        $result = SecurityBL::login($request->all());
-
-        
-        switch ($result) {
-            case LoginResult::SUCCESS:
-                return redirect('home');
-            case LoginResult::INVALID_USER:
-                throw ValidationException::withMessages(['email' => 'El usuario no existe.']);
-                break;
-            case LoginResult::INVALID_PASSWORD:
-                throw ValidationException::withMessages(['password' => 'Contrase&ntilde;a incorrecta.']);
-                break;
-            case LoginResult::LOCKED_OUT:
-                throw ValidationException::withMessages(['locked' => 'Usuario bloqueado. Contacte al administrador.']);
-                break;
-        }
     }
 }
