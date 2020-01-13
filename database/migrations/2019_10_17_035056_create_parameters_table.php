a4faea2fa0600ee98ab24d5519247c8cca2dbd0b
hinedavid <null>
2020-01-13 03:58:47 +0000

    commit parcial, importar excel users

--- database/migrations/2019_10_17_035056_create_parameters_table.php
+++ database/migrations/2019_10_17_035056_create_parameters_table.php
@@ -13,7 +13,7 @@ class CreateParametersTable extends Migration
      */
     public function up()
     {
-        Schema::create('macroprocesses', function (Blueprint $table) {
+        Schema::create('parameters', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateParametersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('macroprocesses');
+        Schema::dropIfExists('parameters');
     }
 }
--- database/migrations/2019_10_22_052248_create_subparameters_table.php
+++ database/migrations/2019_10_22_052248_create_subparameters_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLevelsTable extends Migration
+class CreateSubparametersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateLevelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('levels', function (Blueprint $table) {
+        Schema::create('subparameters', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->bigInteger('macroprocess_id');
+            $table->bigInteger('parameter_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateLevelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('macroprocess');
+        Schema::dropIfExists('subparameters');
     }
 }
